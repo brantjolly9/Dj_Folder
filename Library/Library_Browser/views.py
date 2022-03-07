ba48d8387dbb8c4d6f3f9099dc0b6dcab86ff5f4
brantjolly9 <brant.jolly@gmail.com>
2022-03-07 16:03:36 -0500

    First Migration

--- Library/Library_Browser/views.py
+++ Library/Library_Browser/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def authors_home():
+def index(request):
+    context = {}
     
+    return render(request, 'browser/index.html', context=context)
+    
+def authors_home(request):
+    context = {}
+    return render(request, 'browser/author_home.html', context=context)
+
+def author_detail(request):
     pass
 
-def author_detail():
+def books_home(request):
     pass
 
-def books_home():
-    pass
-
-def book_detail():
-    pass
-
-def index():
+def book_detail(request):
     pass
